a859c647c9e2ee5cac9358c92e05f77481e4cb30
Hyeonsu Kim <31005love@gmail.com>
2026-09-15 19:37:59 +0900

    local

--- Library.py
+++ Library.py
@@ -4,8 +4,9 @@ class Library:
         self.author = author
         self.publisher = publisher
 
-    def addBook(self,title,author):
+    def addBook(self,title,author,pulbisher):
         # 1. 책의 이름, 저자, 출판사를 입력받기
+        #   1-1. 시리얼넘버 부여
         # 2. 데이터베이스에 저장
         # 3. 저장 후 저장되었습니다. 출력
 
@@ -18,8 +19,7 @@ class Library:
     def searchBook(self,title,author):
         #1. 책이나 저자의 이름을 입력받기.
         # 2. 리이브러리에서 데이터를 불러와 콘솔에 츨력
-        # 3. 대출 선택지 출력.
-        # 4. 이후 borrowed 매서드로 연결
+       
     
     def Borrow(self, title, borrower):
             #1.이름이나 저자를 입력받고
@@ -28,3 +28,9 @@ class Library:
             # 4. borrowed 라면 이미 대출된 책입니다. 프린트
             # 5.borrroed 되지 않았다면 사용자의 정보를 입력받은 후 대출처리
             #   5-1 대출처리 후 대출처리 되었습니다. 출력 
+
+    def return(self, borrower):
+            # 1. 빌려간사람의 이름을 입력받고
+            # 2. 그 사람의 대출 목록을 출력
+            # 3. 출력값중 선택된 입력을 반납처리
+        
--- book.py
+++ book.py
@@ -1,13 +1,10 @@
 class Book:
-    def __init__(self, title, author):
+    def __init__(self, title, author, publisher):
         self.title = title
         self.author = author
-        self.isBorrowed =  False
-        self.borrower = None
+        self.publisher = publisher
 
     def show(self):
         print("Name:",self.title)
         print("Author:",self.author)
-        if self.isBorrowed == True:
-            return 
-        print("Borrower:",self.borrower)
+        print("Publisher:",self.publisher)
